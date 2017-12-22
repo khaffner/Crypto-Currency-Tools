dfcd4f0baa2575235c609aa565118de08534de69
khaffner <khaffner@gmail.com>
2017-12-22 10:04:58 +0100

    Added Invoke-Blockmining(demo)
    Just a small script to easily illustrate how mining works

---
+++ Invoke-Blockmining(demo).ps1
@@ -0,0 +1,18 @@
+﻿$n = 0
+while($true) {
+    $Block = @{
+        'PreviusHash'  = '000041635ab87bdb0807ad8a978e38bf84a03a9a7f7961777db0954aba0d1788'
+        'Difficulty'   = '0000'
+        'Transactions' = 'Kevin pays 1Btc to Magnus, Magnus pays 0,5Btc to Peter, I will get 12.5Btc as reward.'
+        'Nonce'        = $n
+    }
+    $Block | Out-File "$env:TEMP\block.txt" -Force
+    $BlockHash = Get-FileHash "$env:TEMP\block.txt" -Algorithm SHA256 | select -ExpandProperty Hash
+    $Difficulty = $Block.Difficulty
+    if($BlockHash -like "$Difficulty*") {
+        return $BlockHash
+    }
+    else {
+        $n++
+    }
+}
